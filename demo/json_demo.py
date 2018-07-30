a10716353e2e6148789770665bd7395885645bbb
haibo.wang <haibo.wang@yulore.com>
2018-07-30 16:42:19 +0800

    http_server_demo

--- demo/json_demo.py
+++ demo/json_demo.py
@@ -3,3 +3,14 @@ import json
 over_flag = dict()
 over_flag['shopid'] = 'test'
 print(json.dumps(over_flag, separators=(',',':'), ensure_ascii=False))
+
+class DateEncoder(json.JSONEncoder):  
+    def default(self, obj):  
+        if isinstance(obj, datetime.datetime):  
+            return obj.strftime('%Y-%m-%d %H:%M:%S')  
+        elif isinstance(obj, date):  
+            return obj.strftime("%Y-%m-%d")  
+        else:  
+            return json.JSONEncoder.default(self, obj) 
+
+print(json.dumps(over_flag, separators=(',',':'), ensure_ascii=False, cls=DateEncoder))
